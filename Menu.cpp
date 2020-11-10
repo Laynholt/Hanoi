6e83fa15b1d3ba4215c82ce55105ae696a6c77cd
Riw Royer <ne.free532@gmail.com>
2020-11-10 23:34:17 +0300

    Добавлено отображение пунктов в настройках

--- Menu.cpp
+++ Menu.cpp
@@ -230,6 +230,67 @@ void Menu::settings()
 	bool go_to_menu = false;
 	_old_choose_scroll = -1;
 
+	sf::RectangleShape rect_count_disks;
+	std::vector<sf::RectangleShape> rect_nums_disks(4);
+	sf::RectangleShape rect_3d;
+	sf::RectangleShape rect_music;
+	std::vector <sf::RectangleShape> rect_on_off_3d(2);
+	std::vector <sf::RectangleShape> rect_on_off_music(2);
+
+	std::vector<sf::Color> col;
+
+	col.push_back(sf::Color(0, 162, 232, 255));
+	col.push_back(sf::Color(232, 220, 0, 255));
+	col.push_back(sf::Color(34, 177, 76, 255));
+	col.push_back(sf::Color(150, 16, 106, 255));
+
+	col.push_back(sf::Color(0, 64, 128, 255));
+	col.push_back(sf::Color(128, 64, 0, 255));
+
+	col.push_back(sf::Color(138, 234, 152, 255));
+	col.push_back(sf::Color(200, 188, 241, 255));
+
+	// Статичные
+	// Плашка количество дисков
+	rect_count_disks.setSize(sf::Vector2f(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 6));
+	rect_count_disks.setPosition(sf::Vector2f(SCREEN_WIDTH / 4, SCREEN_HEIGHT / 12));
+	rect_count_disks.setFillColor(sf::Color(255, 137, 39, 255));
+
+	// Плашка 3D
+	rect_3d.setSize(sf::Vector2f(SCREEN_WIDTH / 4 - 10, SCREEN_HEIGHT / 6));
+	rect_3d.setPosition(sf::Vector2f(SCREEN_WIDTH / 4, SCREEN_HEIGHT / 2));
+	rect_3d.setFillColor(sf::Color(200, 191, 231, 255));
+
+	// Плашка Музыки
+	rect_music.setSize(sf::Vector2f(SCREEN_WIDTH / 4 - 10, SCREEN_HEIGHT / 6));
+	rect_music.setPosition(sf::Vector2f(SCREEN_WIDTH / 2 + 10, SCREEN_HEIGHT / 2));
+	rect_music.setFillColor(sf::Color(163, 73, 164, 255));
+
+	// Динамические
+	for (int16_t i = 0; i < 4; i++)
+	{
+		rect_nums_disks[i].setSize(sf::Vector2f(SCREEN_HEIGHT / 6, SCREEN_HEIGHT / 6 - 10));
+		rect_nums_disks[i].setPosition(sf::Vector2f((SCREEN_HEIGHT / 6 + 10) * i + SCREEN_WIDTH / 4 + SCREEN_HEIGHT / 12, 7 * SCREEN_HEIGHT / 24));
+		rect_nums_disks[i].setFillColor(col[i]);
+	}
+
+	float _x;
+	for (int16_t i = 0; i < 2; i++)
+	{
+		rect_on_off_3d[i].setSize(sf::Vector2f(SCREEN_HEIGHT / 6, SCREEN_HEIGHT / 6 - 10));
+
+		_x = SCREEN_WIDTH / 4 + rect_3d.getSize().x / 10 + i * (rect_on_off_3d[0].getSize().x + 10);
+
+		rect_on_off_3d[i].setPosition(sf::Vector2f(_x, 17 * SCREEN_HEIGHT / 24));
+		rect_on_off_3d[i].setFillColor(col[4 + i]);
+
+		rect_on_off_music[i].setSize(sf::Vector2f(SCREEN_HEIGHT / 6, SCREEN_HEIGHT / 6 - 10));
+		rect_on_off_music[i].setPosition(sf::Vector2f(_x + rect_3d.getSize().x + 20, 17 * SCREEN_HEIGHT / 24));
+		rect_on_off_music[i].setFillColor(col[6 + i]);
+	}
+
+	std::wstring str;
+
 	while (window.isOpen() && !go_to_menu)
 	{
 		while (window.pollEvent(event))
@@ -249,6 +310,104 @@ void Menu::settings()
 		window.clear();
 
 		window.draw(sprite);
+
+		// Рисуем прямоугольник Количества дисков и надпись
+		window.draw(rect_count_disks);
+		if (!font_error)
+		{
+			str = L"Количество дисков";
+			text.setString(str);
+			text.setPosition(sf::Vector2f(SCREEN_WIDTH / 2 - text.getCharacterSize() * str.length() / 4 - 30,
+				rect_count_disks.getSize().y - 40));
+			window.draw(text);
+		}
+
+		// Рисуем прямоугольник 3Д и надпись
+		window.draw(rect_3d);
+		if (!font_error)
+		{
+			str = L"3D";
+			text.setString(str);
+			text.setPosition(sf::Vector2f(rect_3d.getPosition().x - text.getCharacterSize() * str.length() / 4 + rect_3d.getSize().x / 2,
+				rect_3d.getPosition().y + 10));
+			window.draw(text);
+		}
+
+		// Рисуем прямоугольник Музыка и надпись
+		window.draw(rect_music);
+		if (!font_error)
+		{
+			str = L"Музыка";
+			text.setString(str);
+			text.setPosition(sf::Vector2f(rect_music.getPosition().x + text.getCharacterSize() * str.length() / 4 - 50,
+				rect_music.getPosition().y + 10));
+			window.draw(text);
+		}
+
+		// Рисуем выбор количества дисков
+		for (int16_t i = 0; i < 4; i++)
+		{
+			if (i == _old_choose_disk)
+			{
+				rect_nums_disks[i].setOutlineThickness(5);
+				rect_nums_disks[i].setOutlineColor(sf::Color::White);
+			}
+			else
+			{
+				rect_nums_disks[i].setOutlineThickness(0);
+			}
+
+			window.draw(rect_nums_disks[i]);
+
+			if (!font_error)
+			{
+				str = std::to_wstring(3 + i);
+				text.setString(str);
+				text.setPosition(sf::Vector2f(rect_nums_disks[i].getPosition().x - text.getCharacterSize() * str.length() / 4 + rect_nums_disks[i].getSize().x / 2,
+					rect_nums_disks[i].getPosition().y + 10));
+				window.draw(text);
+			}
+		}
+
+		// Рисуем вкл откл ползунков
+		for (int16_t i = 0, j = 1; i < 2; i++, j--)
+		{
+			if (i == _old_choose_3d)
+			{
+				rect_on_off_3d[i].setOutlineThickness(5);
+				rect_on_off_3d[i].setOutlineColor(sf::Color::White);
+			}
+			else { rect_on_off_3d[i].setOutlineThickness(0); }
+
+			if (i == _old_choose_music)
+			{
+				rect_on_off_music[i].setOutlineThickness(5);
+				rect_on_off_music[i].setOutlineColor(sf::Color::White);
+			}
+			else { rect_on_off_music[i].setOutlineThickness(0); }
+
+			// 3D надпись
+			window.draw(rect_on_off_3d[i]);
+			if (!font_error)
+			{
+				str = std::to_wstring(j);
+				text.setString(str);
+				text.setPosition(sf::Vector2f(rect_on_off_3d[i].getPosition().x - text.getCharacterSize() * str.length() / 4 + rect_on_off_3d[i].getSize().x / 2,
+					rect_on_off_3d[i].getPosition().y + 10));
+				window.draw(text);
+			}
+
+			// Музыка надпись
+			window.draw(rect_on_off_music[i]);
+			if (!font_error)
+			{
+				str = std::to_wstring(j);
+				text.setString(str);
+				text.setPosition(sf::Vector2f(rect_on_off_music[i].getPosition().x - text.getCharacterSize() * str.length() / 4 + rect_on_off_music[i].getSize().x / 2,
+					rect_on_off_music[i].getPosition().y + 10));
+				window.draw(text);
+			}
+		}
 
 		window.display();
 	}
